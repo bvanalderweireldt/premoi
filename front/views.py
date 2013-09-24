5ab09d0d13746df021c2a62e154cc7f7f758e845
bvanalder <benoit.vanalder@ecommerceaccel.com>
2013-09-24 20:58:13 +1000

    start slider work

--- front/views.py
+++ front/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from front.models import Slider
 def home(request):
-    return render(request, 'home.html', {})
+    sliders = Slider.objects.order_by('id')
+    return render(request, 'home.html', { 'sliders' : sliders })
